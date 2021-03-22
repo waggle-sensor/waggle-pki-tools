3e2ad52602029c93fcc209d518ecf1d75b3e2c89
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-03-22 14:47:43 -0500

    merge into one script and create ca certificate configmap

--- tls/create-beehive-credentials.sh
+++ tls/create-beehive-credentials.sh
@@ -1,18 +1,69 @@
 #!/bin/bash
 
-source "$(dirname $0)/common.sh"
+CAKEYFILE="ca/cakey.pem"
+CACERTFILE="ca/cacert.pem"
+
+init_ca() {
+    mkdir -p "$(dirname $CAKEYFILE)"
+
+    # create ca key / cert if key doesn't exist
+    if ! test -e "$CAKEYFILE"; then
+        openssl req -x509 \
+            -nodes -newkey rsa:4096 -keyout "$CAKEYFILE" -out "$CACERTFILE" \
+            -sha256 -days 36500 -subj "/CN=beekeeper"
+    fi
+
+    cat > $(dirname "$CACERTFILE")/beehive-ca-certificate.yaml <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: beehive-ca-certificate
+data:
+  cacert.pem: |
+$(awk '{print "    " $0}' ca/cacert.pem)
+EOF
+}
+
+sign_credentials() {
+    name="$1"
+    keyfile="ca/$name.key.pem"
+    csrfile="ca/$name.csr.pem"
+    certfile="ca/$name.cert.pem"
+
+    mkdir -p "$(dirname $keyfile)"
+
+    # create signing request
+    openssl req -new \
+        -nodes -newkey rsa:4096 -keyout "$keyfile" -out "$csrfile" \
+        -subj "/CN=$name"
+
+    # sign request using ca
+    openssl x509 -req \
+        -in "$csrfile" -out "$certfile" \
+        -CAkey "$CAKEYFILE" -CA "$CACERTFILE" -CAcreateserial \
+        -sha256 -days 365
+
+    # also create kuberenetes secret resource for immediate use
+    # TODO(sean) think about whether this is primary thing we want to ship. it's a flexible 
+    # format which already includes the essentials, room to grow and is also immediately
+    # usable by kubernetes
+    mkdir -p secrets
+
+    secret_name="${name}-tls-secret"
+    cat > "secrets/${secret_name}.yaml" <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${secret_name}
+type: Opaque
+data:
+  cacert.pem: $(base64 "$CACERTFILE")
+  cert.pem: $(base64 "$certfile")
+  key.pem: $(base64 "$keyfile")
+EOF
+
+    rm -f "$keyfile" "$csrfile" "$certfile"
+}
 
 init_ca
-
-# create server credentials
 sign_credentials beehive-rabbitmq
-
-# create client credentials
-sign_credentials beehive-message-logger
-sign_credentials beehive-influxdb-loader
-sign_credentials beehive-message-generator
-
-# TODO consider approach for easier management of client services
-# within cluster. this may be:
-# 1. have intermediate ca used by beehive just for its own services
-# 2. use username / random passwords in rabbitmq
