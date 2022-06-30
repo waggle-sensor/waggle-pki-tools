c9b6f148990a4ad5cb555cd146f0acd9f1565e59
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-06-30 11:37:04 -0500

    changed to repo dir at start of script

--- create-credentials-for-beehive.sh
+++ create-credentials-for-beehive.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+cd $(dirname "${0}")
+
 (cd tls && ./create-beehive-credentials.sh)
 (cd ssh && ./create-beehive-credentials.sh)
 
--- create-credentials-for-nodes.sh
+++ create-credentials-for-nodes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+cd $(dirname "${0}")
+
 for nodeID in $*; do
     name="node-$nodeID"
 
